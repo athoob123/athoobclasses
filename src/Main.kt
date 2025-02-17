368036690bdb1eef0c7f93367e71d0a481098c3e
athoob123 <athoob.alroumi@gmail.com>
2025-02-17 16:34:20 +0300

    Task done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
-fun main() {
-    println("Hello World!")
+
+class Book {
+        var title: String = "House"
+        val author: String = "Bobby"
+        var yearPublished: Int = 2001
+    }
+fun main () {
+    val myBook = Book()
+
+    myBook.title = "House"
+    myBook.author = "Bobby Lee"
+    myBook.yearPublished = 2001
 }
+
+class Book(val title: String, val author: String, val yearPublished: Int) {
+    fun getBookInfo(): String {
+        return "$title by $author ($yearPublished)"
+    }
+fun main() {
+    val myBook = Book("House", "Bobby", 2001)
+
+    println("Title: ${myBook.title}")
+    println("Author: ${myBook.author}")
+    println("Year Published: ${myBook.yearPublished}")
+}
+
+
+
+
